f5e9c7ee23577b067e0c9b775fc82a1e041cc740
ZedRover <29392414+Roverzed@users.noreply.github.com>
2021-04-26 15:26:43 +0800

    nn

---
+++ RNN_wy/neural_nets/__init__.py
@@ -0,0 +1,11 @@
+"""A module of basic building blcoks for constructing neural networks"""
+from . import utils
+from . import losses
+from . import activations
+from . import schedulers
+from . import optimizers
+from . import wrappers
+from . import layers
+from . import initializers
+from . import modules
+from . import models
---
+++ RNN_wy/neural_nets/models/__init__.py
@@ -0,0 +1,3 @@
+from .vae import *
+from .wgan_gp import *
+from .w2v import *
